6eb0700a04bae550e5c2f78e446bb20a42dc4688
whhp <maryane.castro11@aluno.ifce.edu.br>
2023-01-10 20:37:26 -0300

    .

--- 3.aula/src/ex4.java
+++ 3.aula/src/ex4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ex1 {
+public class ex4 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("Digite sua nota1: ");
