07e7b35e17b9d7a0f9dccbb8cd6bb9ee30849ded
whhp <maryane.castro11@aluno.ifce.edu.br>
2023-01-10 20:36:13 -0300

    .

---
+++ 3.aula/src/ex1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite sua nota1: ");
+        double n1 = entrada.nextDouble();
+        System.out.print("Digite sua nota2: ");
+        double n2 = entrada.nextDouble();
+
+        double media = (n1 + n2) / 2;
+        if (media> 6){
+            System.out.println("Aprovado");
+        }
+        else if (media < 6){
+            System.out.println("Reprovado");
+        }
+
+    }
+}
