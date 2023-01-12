d1b9e0110a6f4d13b4667179712d93054a119304
whhp <maryane.castro11@aluno.ifce.edu.br>
2023-01-11 21:00:50 -0300

    .

--- extras/calculate/src/calculate.java
+++ extras/calculate/src/calculate.java
@@ -6,15 +6,25 @@ public class calculate {
         
         System.out.println("1 - soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão");
         System.out.println("Escolha uma opção: ");
-        String aux = input.nextLine();
+        int aux = input.nextInt();
 
-        if (aux == "1"){
+        if (aux == 1){
             soma();
+        } else if (aux == 2){
+            sub();
+        } else if(){
+            mul();
         }
     }
 
     static void soma(){
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("number 1: ");
+        double n1 = input.nextDouble();
+        System.out.print("number 2: ");
+        double n2 = input.nextDouble();
+
+        System.out.println(n1 + n2);
     }
 
     static void sub(){
