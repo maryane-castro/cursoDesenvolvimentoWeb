7b1f47531f379ee4a018f650f3c8bca849d9d8d7
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-12 19:57:37 -0300

    aula5
    classes, objetos, atributos

---
+++ 5.aula/src/ContChama.java
@@ -0,0 +1,23 @@
+/*Mesma coisa em python!! */
+public class ContChama {
+    private String nome;                      //criação de variaveis
+    private double saldoBancario;
+
+
+
+
+    public void setNome(String nome){       //inicia a variavel, como se fosse o __init__
+        this.nome = nome;
+    }
+    public String getNome(){                //aqui é como se fosse uma função
+        return nome+"21";
+    }
+
+    public void setSaldo(double saldoBancario){
+        this.saldoBancario = saldoBancario;
+    }
+
+    public double getSaldo(){
+        return saldoBancario;
+    }
+}
---
+++ 5.aula/src/Conta.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+/*
+SET pega a string
+GET mostra a string
+ */
+public class Conta {
+    public static void main(String[] args) {
+        ContChama conta1 = new ContChama();
+        
+        System.out.printf("Nome da conta: %s%n", conta1.getNome());
+        System.out.println(); 
+
+        
+
+        Scanner input = new Scanner(System.in);
+
+        /* 
+        String teste = input.nextLine();
+        System.out.println(teste);
+        */
+
+
+        System.out.print("Digite uma String para ser setada: ");
+        conta1.setNome(input.nextLine());
+        ContChama conta2 = conta1; //obj2 recebe o valor do objj2
+        System.out.println(conta2.getNome()); //aqui ele mostra o valor do obj2
+
+        System.out.printf("A String setada foi: %s ",conta1.getNome());
+        
+        /*ambos objetos vão ter os mesmo valores, já que é o mesmo espaço de memória!! */
+        
+
+        conta1.setSaldo(21);
+        System.out.println(conta1.getSaldo());
+        
+    }
+}
+
