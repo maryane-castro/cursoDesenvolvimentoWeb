62b69e70b0e9f454f81a1a37faeba69699385a5c
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-16 20:20:15 -0300

    .

---
+++ 6.aula/src/App.java
@@ -0,0 +1,81 @@
+public class App {
+    public static void main(String[] args) {
+        /* 
+        String s1 = "hello";
+        
+        System.out.printf("String s1: %s%n", s1);
+
+
+        System.out.printf("Tamanho de S1: %d%n", s1.length());  //conta quantos caracteres existe, esaços contam tambem
+
+        System.out.printf("Char na posição 0: %c%n", s1.charAt(0)); //mostra qual caractere está naquela posição
+        System.out.printf("Char na posição 0: %c%n", s1.charAt(5));
+
+
+        int posicao = 3;
+        System.out.printf("char no indice %d: %c%n", posicao, s1.charAt(posicao));
+
+
+        posicao += 1;
+        System.out.printf("char no indice %d: %c%n", posicao, s1.charAt(posicao));
+        */
+/* 
+        String s1 = new String("hello");
+        String s2 = "hello";
+
+
+
+        if (s1.equals("hello")){
+            System.out.println("s1 equal \"hello\" ");
+        } else{ 
+                System.out.println("s1 não é igual  \"hello\" ");
+        }
+
+
+        if (s1 == "hello"){
+            System.out.println("if: é igual");
+        } else {
+            System.out.println("else: não é igual");
+        }
+
+        if (s2.equals("hello")){
+            System.out.println("sim"); // *
+        } else {
+            System.out.println("nao");
+        }
+
+        if (s2 == "hello"){
+            System.out.println("sim"); // *
+        } else {
+            System.out.println("não");
+        }
+
+
+
+
+
+        --------------------------------------------------------------------- 
+
+        String s3 = "Happy";
+        String s4 = "happy";
+
+        if (s3.equalsIgnoreCase(s4)){    //ignora diferença do H e h
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+*/
+/* 
+        String s1 = new String("hello");  //compara qual é o maior
+        String s2 = "goodbye";
+        String s3 = "Happy Birthday";
+        String s4 = "happy birthday";
+        
+        System.out.printf("%d", s1.compareTo(s2));
+*/
+
+
+
+        
+    }
+}
---
+++ 7.aula/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) {
+        String s3 = "Happy Birthday";
+        String s4 = "happy birthday";
+
+        if (s3.regionMatches( 0, s4, 0, 5)){
+            System.out.println("iguais");
+        } else{
+            System.out.println("não iguais");
+        }
+
+        if (s3.regionMatches(true, 0, s4, 0, 5)){ //ignorou as letras minusculas (true ignore)
+            System.out.println("true");
+        } else{
+            System.out.println("não igual");
+        }
+
+        String s1 = "started";
+        String s2 = "saved";
+
+        if (s1.startsWith("st")) {
+            System.out.println("verdadeiro");
+        }
+
+        if (s2.startsWith("st")) {
+            System.out.println("verdadeiro");
+        } else {
+            System.out.println("false");
+        }
+
+        if (s1.endsWith("ed")){
+            System.out.println("verdadeiro");
+        }
+    }
+}
