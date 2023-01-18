0ca08d594227b60639b7ee7f96d1dd6f4ff15f56
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-18 19:51:32 -0300

    Update App.java

--- 8.aula/src/App.java
+++ 8.aula/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         /*int num = 1;
@@ -5,9 +7,19 @@ public class App {
             System.out.println(num);
             ++num;
         } while (num <= 10);*/
-
-
-
         
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite a quantidade de funcionários: ");
+        int func = input.nextInt();
+        int aux = 1;
+        double soma = 0;
+        while (aux <= func){
+            System.out.printf("Salário do funcionario %d: ", aux);
+            double salario = input.nextDouble();
+            soma = soma + salario;
+            ++aux;
+        }
+        System.out.printf("A soma dos salários dos funcionários é: R$ %.2f ", soma);
+
     }
 }
