385faaf6167e6bbe89314192ccb67208ab470da8
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-18 19:08:55 -0300

    create new 8.aula

---
+++ 8.aula/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*int num = 1;
+        do {
+            System.out.println(num);
+            ++num;
+        } while (num <= 10);*/
+
+
+
+        
+    }
+}
