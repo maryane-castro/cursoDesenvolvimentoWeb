607a32b0bfc9c12a9b2756ac70dc6795e98dae25
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-19 20:30:00 -0300

    Update App.java

--- 9.aula/src/App.java
+++ 9.aula/src/App.java
@@ -1,5 +1,16 @@
+//VETORES
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+
+        int [] listaA = new int[12];
+        String [] listaB = new String[10];
+
+        listaA [1] = 12;
+        listaB [2] = "UEPA";
+        System.out.println(listaA[1]);
+        System.out.println(listaB[2]);
+
+
     }
 }
