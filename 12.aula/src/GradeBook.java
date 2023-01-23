8f2fee7e9aad60026c65555f67924b10aac4964d
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-23 19:23:43 -0300

    .

---
+++ 12.aula/src/GradeBook.java
@@ -0,0 +1,14 @@
+public class GradeBook {
+
+    private String nomeCurso;
+    double [] listaNotas;
+
+
+    public String getNomeCurso(){
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso){
+        this.nomeCurso = nomeCurso;
+    }
+}
