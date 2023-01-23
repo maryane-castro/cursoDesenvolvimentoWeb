e7984a01805b6fb08bbb9ed8344fc3ffc68f0499
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-23 20:50:56 -0300

    update

--- 12.aula/src/GradeBook.java
+++ 12.aula/src/GradeBook.java
@@ -3,6 +3,17 @@ public class GradeBook {
     private String nomeCurso;
     private double [] notasAlunos;
 
+    /*private int umDois = 0;
+    private int doisTres = 0;
+    private int tresQuatro = 0;
+    private int quatroCinco = 0;
+    private int cincoSeis = 0;
+    private int seisSete = 0;
+    private int seteOito = 0;
+    private int oitoNove = 0;
+    private int noveDez = 0;
+    private int zeroUm = 0;*/
+
 
     public String getNomeCurso(){
         return nomeCurso;
@@ -42,4 +53,37 @@ public class GradeBook {
         }
         return maiorNota;
     }
+
+
+    /*DISTRIBUIÇÃO */
+
+    public void motraDistribuicaoNotas(){
+        int [] frequenciaFaixasNotas = new int [10];
+
+        for (double nota : notasAlunos){
+            if (0 <= nota && nota < 1){
+                frequenciaFaixasNotas[0]++;
+            } else if (1 <= nota && nota < 2){
+                frequenciaFaixasNotas[1]++;
+            } else if (2 <= nota && nota < 3){
+                frequenciaFaixasNotas[2]++;
+            } else if (3 <= nota && nota < 4){
+                frequenciaFaixasNotas[3]++;
+            } else if (4 <= nota && nota < 5){
+                frequenciaFaixasNotas[4]++;
+            } else if (5 <= nota && nota < 6){
+                frequenciaFaixasNotas[5]++;
+            } else if (6 <= nota && nota < 7){
+                frequenciaFaixasNotas[6]++;
+            } else if (7 <= nota && nota < 8){
+                frequenciaFaixasNotas[7]++;
+            } else if (8 <= nota && nota < 9){
+                frequenciaFaixasNotas[8]++;
+            } else if (9 <= nota && nota <= 10){
+                frequenciaFaixasNotas[9]++;
+            }
+        }
+    }
+
+    
 }
