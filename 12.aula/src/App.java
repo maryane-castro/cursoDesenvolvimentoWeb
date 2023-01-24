17be2824c62dcc3e21cd4f3a61079880b8077fed
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-24 19:18:09 -0300

    Update App.java

--- 12.aula/src/App.java
+++ 12.aula/src/App.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args)  {
-        int quantidadedeNotas = 0;
         
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Digite a quantidade de notas: ");
-        int quantidadeNotas = input.nextInt();
-
-        GradeBook cursoJava = new GradeBook(quantidadeNotas);
 
     }
 }
