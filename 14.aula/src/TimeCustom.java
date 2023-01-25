2fde439068fa70545a9f9f7d3ca66fb97d481fae
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-25 19:23:07 -0300

    update

---
+++ 14.aula/src/TimeCustom.java
@@ -0,0 +1,36 @@
+public class TimeCustom {
+    private int hour; 
+    private int minute;
+    private int second; 
+
+    public TimeCustom() {
+        this(0, 0, 0); 
+    }
+
+
+    public TimeCustom(int hour) {
+        this(hour, 0, 0); 
+    }
+
+
+    public TimeCustom(int hour, int minute) {
+        this(hour, minute, 0); 
+    }
+
+
+    public TimeCustom(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute / 60;
+        this.second = second / (60 * 60); // transformando os segundos na unidade de horas
+    }
+
+
+    public TimeCustom(TimeCustom time) {
+        this(time.hour, time.minute, time.second);
+    }
+
+    public void mostraTempo(){
+        System.out.printf("Hora armazenada: %d:%d:%d", hour, minute, second);
+    }
+
+}
