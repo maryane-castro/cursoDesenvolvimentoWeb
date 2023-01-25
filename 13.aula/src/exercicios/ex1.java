1e5477479fe3a5069569eff22788b52d3262fb21
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-24 21:19:49 -0300

    Create ex1.java

---
+++ 13.aula/src/exercicios/ex1.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class ex1 {
+    public static void main(String[] args) {
+        //sobrecarga de metodos
+
+        /*formatação em metodos
+        return String.format("Digite a formatação %d%n")*/
+    }
+}
