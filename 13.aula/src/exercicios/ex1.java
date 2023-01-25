9166a9a0b994162f5bcb57b2d7365bb1aabfad0a
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-24 22:03:05 -0300

    Update ex1.java

--- 13.aula/src/exercicios/ex1.java
+++ 13.aula/src/exercicios/ex1.java
@@ -6,5 +6,22 @@ public class ex1 {
 
         /*formatação em metodos
         return String.format("Digite a formatação %d%n")*/
+
+        double [] notaAlunos = {0.8};
+        int [] frequenciaFaixasNotas = new int [10]; //10 linhas 
+
+        for (double nota : notaAlunos){
+            if (nota < 10){
+                frequenciaFaixasNotas[(int) nota]++;
+                System.out.println(nota);
+            }  else {
+                frequenciaFaixasNotas[9]++;
+            }
+        }
+
+        System.out.println("Distribuição das notas: ");
+        for (int i = 0; i < frequenciaFaixasNotas.length; i++){
+            System.out.printf("Quantidade de notas entre %d e %d: %d%n", i, i+1, frequenciaFaixasNotas[i]);
+        }
     }
 }
