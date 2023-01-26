c1b3eb0ec639a14ba1bc8fa9439c09b3b21466cd
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-26 19:21:57 -0300

    Update Data.java

--- 15.aula/src/Data.java
+++ 15.aula/src/Data.java
@@ -4,21 +4,28 @@ public class Data {
      private int year; // any year
      private static final int[] daysPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
      
-    public Data(int month, int day, int year) {
-     this.month = month;
-     this.day = day;
-     this.year = year;
-     System.out.printf("Date object constructor for date %s%n", this);
-     }
-     // return a String of the form month/day/year
-     public String toString() {
-    return String.format("%d/%d/%d", month, day, year);
-     }
-
-     public void setData(int day, int month, int year){
+    public Data(int month, int day, int year) { //colocar
         this.month = month;
         this.day = day;
         this.year = year;
+        System.out.printf("Date object constructor for date %s%n", this);
+
+     
      }
+
+
+    public void setData(int day, int month, int year){ //pegar
+        this.month = month;
+        this.day = day;
+        this.year = year;
     }
+
+
+
+    public String toString() {          //toString para retornar em outra class
+        return String.format("%d/%d/%d", month, day, year);
+     }
+
+     
+}
     
