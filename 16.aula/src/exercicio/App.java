cd53ff12880db39c268dbb599b6c5d08b46d1150
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-26 20:56:52 -0300

    update

---
+++ 16.aula/src/exercicio/App.java
@@ -0,0 +1,13 @@
+/*Construa uma hierarquia de herança para estudantes em uma 
+universidade. Use Student como superclasse da hierarquia e 
+crie como subclasses diretas dela as classes 
+UndergraduateStudent e GraduateStudent. Adicione as 
+classes DoctoralStudent e MastersStudent como 
+subclasses de GraduateStudent. Em seguida, defina um 
+programa principal para usar as classes que você definiu.*/
+
+package exercicio;
+
+public class App {
+    
+}
---
+++ 16.aula/src/exercicio/Student.java
@@ -0,0 +1,5 @@
+package exercicio;
+//SUPERCLASS
+public class Student {
+    
+}
